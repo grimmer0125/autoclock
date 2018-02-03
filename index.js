3974d1d9f937d95961c9ac58193fcbb1811a2e27
Grimmer <grimmer0125@gmail.com>
2018-02-03 14:54:26 +0800

    Actively force the page after clicking/submitting. Fix the bug like the below case, 1. after clicking 1st clock in button, wait for auto-refresh, but not enough. 2. input new clock out time 3. auto-refresh due to form-submit 4. click clock out button but using default date

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const notifier = require('node-notifier');
 const Nightmare = require('nightmare');
 
 // const nightmare = Nightmare({ show: true, typeInterval: 20, openDevTools:true });
+// const nightmare = Nightmare({ show: true, typeInterval: 20, openDevTools:true });
+// const nightmare = Nightmare({ show: true, openDevTools:true });
 const nightmare = Nightmare({ show: false, typeInterval: 20 });
+
+// const nightmare = Nightmare({ show: false, typeInterval: 20 });
 
 const fs = require("fs");
 const content = fs.readFileSync("config.json");
@@ -37,6 +41,9 @@ page.on('console', (type, message)=> {
   }
 });
 
+// https://github.com/segmentio/nightmare/issues/854#issuecomment-256658926
+// not work,  for auto refresh from submiting
+
 for (const date of dateArray) {
   // console.log("fill date:", date);
 
@@ -44,28 +51,52 @@ for (const date of dateArray) {
   .evaluate((text) => {
       console.log("start write date::"+ text);
   }, date)
+  .wait("input#d")
+  .wait("input#t")
+  .wait("input#bt_in")
   .type('input#d', "")
   .type('input#d', date)
   .type('input#t', "")
   .type('input#t', clockInTime)
   .click('input#bt_in')
-  .wait(2100)
+
+  // if we use wait, it is still possible that
+  // 1. after clicking 1st clock in button, wait for auto-refresh, but not enough.
+  // 2. input new clock out time
+  // 3. auto-refresh due to form-submit
+  // 4. click clock out button but using default date
+  // .wait(1500) ->
+  .refresh()
+  // .evaluate(waitforunload)
+  // .waitforunload()
+  .wait("input#d")
+  .wait("input#t")
+  .wait("input#bt_out")
+  .wait(300)
   .type('input#d', "")
   .type('input#d', date)
   .type('input#t', "")
   .type('input#t', clockOutTime)
   .click('input#bt_out')
-  .wait(2100)
+  // .wait(100)
+  .refresh()
   .evaluate((text) => {
       console.log("end write date::"+ text)
   }, date)
+   // 1. even without this wait, this script works. this is for lower server workload(too quick)
+   // 2. this wait can let people know the changed result on page,
+   // otherwise the webpage will not update immediately (send command works)
+  .wait(1500); // even without this, this script works. this is for lower server workload(too quick)
+
+
+  // .wait(30000);
 }
 
 page
 .end()
 .then(()=> {
   console.log("auto clock is finished !!!!!, " + handlingDate);
-  notifier.notify('auto clock is finished:' + handlingDate +" !!");
+  notifier.notify('auto clock is finished, final:' + handlingDate +" !!");
 })
 .catch((error) => {
     console.error('auto clock failure:', error);
